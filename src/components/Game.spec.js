917a06542d8ec5ee54a82235f19355c76091bb55
nihemak <nihemak@gmail.com>
2022-01-24 08:15:32 +0900

    Fix test

--- src/components/Game.spec.js
+++ src/components/Game.spec.js
@@ -46,9 +46,8 @@ it("盤面が表示されること", () => {
   const squares = document.querySelectorAll(".square");
   expect(squares.length).toBe(9);
 
-  squares.forEach((square) => {
-    expect(square.innerHTML).toBe("");
-  });
+  const actualBoard = [...squares].map(square => square.innerHTML);
+  expect(actualBoard).toStrictEqual(Array(9).fill(""));
 });
 
 it("ゲームできること", () => {
@@ -62,52 +61,52 @@ it("ゲームできること", () => {
   [
     {
       putIndex: 1,
-      resultStatus: "Next player: O",
-      resultBoard: ["", "X", "", "", "", "", "", "", ""],
+      expectedStatus: "Next player: O",
+      expectedBoard: ["", "X", "", "", "", "", "", "", ""],
     },
     {
       // クリック済みの場合は結果が変わらない
       putIndex: 1,
-      resultStatus: "Next player: O",
-      resultBoard: ["", "X", "", "", "", "", "", "", ""],
+      expectedStatus: "Next player: O",
+      expectedBoard: ["", "X", "", "", "", "", "", "", ""],
     },
     {
       putIndex: 0,
-      resultStatus: "Next player: X",
-      resultBoard: ["O", "X", "", "", "", "", "", "", ""],
+      expectedStatus: "Next player: X",
+      expectedBoard: ["O", "X", "", "", "", "", "", "", ""],
     },
     {
       putIndex: 4,
-      resultStatus: "Next player: O",
-      resultBoard: ["O", "X", "", "", "X", "", "", "", ""],
+      expectedStatus: "Next player: O",
+      expectedBoard: ["O", "X", "", "", "X", "", "", "", ""],
     },
     {
       putIndex: 8,
-      resultStatus: "Next player: X",
-      resultBoard: ["O", "X", "", "", "X", "", "", "", "O"],
+      expectedStatus: "Next player: X",
+      expectedBoard: ["O", "X", "", "", "X", "", "", "", "O"],
     },
     {
       putIndex: 7,
-      resultStatus: "Winner: X",
-      resultBoard: ["O", "X", "", "", "X", "", "", "X", "O"],
+      expectedStatus: "Winner: X",
+      expectedBoard: ["O", "X", "", "", "X", "", "", "X", "O"],
     },
     {
       // 勝敗が決まったら結果が変わらない
       putIndex: 2,
-      resultStatus: "Winner: X",
-      resultBoard: ["O", "X", "", "", "X", "", "", "X", "O"],
+      expectedStatus: "Winner: X",
+      expectedBoard: ["O", "X", "", "", "X", "", "", "X", "O"],
     },
-  ].forEach(({ putIndex, resultStatus, resultBoard }) => {
+  ].forEach(({ putIndex, expectedStatus, expectedBoard }) => {
     act(() => {
       squares[putIndex].dispatchEvent(
         new MouseEvent("click", { bubbles: true })
       );
     });
 
-    expect(status.innerHTML).toBe(resultStatus);
-    squares.forEach((square, index) => {
-      expect(square.innerHTML).toBe(resultBoard[index]);
-    });
+    expect(status.innerHTML).toBe(expectedStatus);
+
+    const actualBoard = [...squares].map(square => square.innerHTML);
+    expect(actualBoard).toStrictEqual(expectedBoard);
   });
 });
 
@@ -130,17 +129,16 @@ it("タイムトラベルできること", () => {
     });
   });
 
-  let resultBoard = ["X", "O", "", "X", "", "", "", "X", "O"];
-  squares.forEach((square, index) => {
-    expect(square.innerHTML).toBe(resultBoard[index]);
-  });
+  let expectedBoard = ["X", "O", "", "X", "", "", "", "X", "O"];
+  let actualBoard = [...squares].map(square => square.innerHTML);
+  expect(actualBoard).toStrictEqual(expectedBoard);
 
   let status = document.querySelector(".game-info div");
   expect(status.innerHTML).toBe("Next player: O");
 
   moves = document.querySelectorAll(".game-info ol li button");
 
-  const resultMoves = [
+  const expectedMoves = [
     "Go to game start",
     "Go to move #1",
     "Go to move #2",
@@ -148,20 +146,18 @@ it("タイムトラベルできること", () => {
     "Go to move #4",
     "Go to move #5",
   ];
-  expect(moves.length).toBe(resultMoves.length);
-  moves.forEach((move, index) => {
-    expect(move.innerHTML).toBe(resultMoves[index]);
-  });
+  expect(moves.length).toBe(expectedMoves.length);
+  const actualMoves = [...moves].map(move => move.innerHTML);
+  expect(actualMoves).toStrictEqual(expectedMoves);
 
   // タイムトラベル実施
   act(() => {
     moves[2].dispatchEvent(new MouseEvent("click", { bubbles: true }));
   });
 
-  resultBoard = ["X", "O", "", "", "", "", "", "", ""];
-  squares.forEach((square, index) => {
-    expect(square.innerHTML).toBe(resultBoard[index]);
-  });
+  expectedBoard = ["X", "O", "", "", "", "", "", "", ""];
+  actualBoard = [...squares].map(square => square.innerHTML);
+  expect(actualBoard).toStrictEqual(expectedBoard);
 
   status = document.querySelector(".game-info div");
   expect(status.innerHTML).toBe("Next player: X");
